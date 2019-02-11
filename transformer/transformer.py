26a43673dfd92221023058f00573f57178d25372
echen94 <chenyiying@ucla.edu>
2019-02-11 00:57:19 -0800

    transformer with german-english translation example

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,9 +1,11 @@
 # reference: http://nlp.seas.harvard.edu/2018/04/03/attention.html#full-model
 
-# install: http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp36-cp36m-linux_x86_64.whl numpy matplotlib spacy torchtext seaborn
+# requirements:
+# pip install torch==0.3.1 numpy matplotlib spacy torchtext==0.2.3 seaborn
+# python -m spacy download en
+# python -m spacy download de 
 
 # Standard PyTorch imports
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -384,7 +386,7 @@ class SimpleLossCompute:
         return loss.data[0] * norm
 
 # Train the simple copy task.
-
+'''
 V = 11
 criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
 model = make_model(V, V, N=2)
@@ -398,7 +400,7 @@ for epoch in range(10):
     model.eval()
     print(run_epoch(data_gen(V, 30, 5), model, 
                     SimpleLossCompute(model.generator, criterion, None)))
-
+'''
 def greedy_decode(model, src, src_mask, max_len, start_symbol):
     memory = model.encode(src, src_mask)
     ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
@@ -413,9 +415,119 @@ def greedy_decode(model, src, src_mask, max_len, start_symbol):
         ys = torch.cat([ys, 
                         torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
     return ys
-
+'''
 model.eval()
 src = Variable(torch.LongTensor([[1,2,3,4,5,6,7,8,9,10]]) )
 src_mask = Variable(torch.ones(1, 1, 10) )
 print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
+'''
+# data loading 
+
+# For data loading.
+from torchtext import data, datasets
+
+if True:
+    import spacy
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
+
+    def tokenize_de(text):
+        return [tok.text for tok in spacy_de.tokenizer(text)]
+
+    def tokenize_en(text):
+        return [tok.text for tok in spacy_en.tokenizer(text)]
+
+    BOS_WORD = '<s>'
+    EOS_WORD = '</s>'
+    BLANK_WORD = "<blank>"
+    SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+    TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD, 
+                     eos_token = EOS_WORD, pad_token=BLANK_WORD)
+
+    MAX_LEN = 100
+    train, val, test = datasets.IWSLT.splits(
+        exts=('.de', '.en'), fields=(SRC, TGT), 
+        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and 
+            len(vars(x)['trg']) <= MAX_LEN)
+    MIN_FREQ = 2
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
+            
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                          self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+def rebatch(pad_idx, batch):
+    "Fix order in torchtext to match ours"
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    return Batch(src, trg, pad_idx)
+
+# GPUs to use
+devices = [0]
+if True:
+    pad_idx = TGT.vocab.stoi["<blank>"]
+    model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
+    model.cuda()
+    criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
+    criterion.cuda()
+    BATCH_SIZE = 12000
+    train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
+                            repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                            batch_size_fn=batch_size_fn, train=True)
+    valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
+                            repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                            batch_size_fn=batch_size_fn, train=False)
+    model_par = nn.DataParallel(model, device_ids=devices)
+None
+
+if False:
+    model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
+            torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+    for epoch in range(10):
+        model_par.train()
+        run_epoch((rebatch(pad_idx, b) for b in train_iter), 
+                  model_par, 
+                  MultiGPULossCompute(model.generator, criterion, 
+                                      devices=devices, opt=model_opt))
+        model_par.eval()
+        loss = run_epoch((rebatch(pad_idx, b) for b in valid_iter), 
+                          model_par, 
+                          MultiGPULossCompute(model.generator, criterion, 
+                          devices=devices, opt=None))
+        print(loss)
+else:
+    model = torch.load("iwslt.pt")
+
+for i, batch in enumerate(valid_iter):
+    src = batch.src.transpose(0, 1)[:1]
+    src_mask = (src != SRC.vocab.stoi["<blank>"]).unsqueeze(-2)
+    out = greedy_decode(model, src, src_mask, 
+                        max_len=60, start_symbol=TGT.vocab.stoi["<s>"])
+    print("Translation:", end="\t")
+    for i in range(1, out.size(1)):
+        sym = TGT.vocab.itos[out[0, i]]
+        if sym == "</s>": break
+        print(sym, end =" ")
+    print()
+    print("Target:", end="\t")
+    for i in range(1, batch.trg.size(0)):
+        sym = TGT.vocab.itos[batch.trg.data[i, 0]]
+        if sym == "</s>": break
+        print(sym, end =" ")
+    print()
+    break
 
