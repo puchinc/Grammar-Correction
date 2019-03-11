321ef138b3e108d279142022e1b5bf5bc9510db4
michaelchen110 <puching110@gmail.com>
2019-03-10 17:26:40 -0700

    save prediction in eval folder

--- transformer/transformer_pred.py
+++ transformer/transformer_pred.py
@@ -109,76 +109,43 @@ def main():
 
     print("Predicting %s %s %s ..." % (DATA, EN_EMB, DE_EMB))
 
+    f_src = open(os.path.join(eval_dir, 
+        '%s.%s.%s.eval.src' % (DATA, EN_EMB, DE_EMB)), 'w+')
+    f_trg = open(os.path.join(eval_dir, 
+        '%s.%s.%s.eval.trg' % (DATA, EN_EMB, DE_EMB)), 'w+')
+    f_pred = open(os.path.join(eval_dir, 
+        '%s.%s.%s.eval.pred' % (DATA, EN_EMB, DE_EMB)), 'w+')
+
     for batch in (rebatch(pad_idx, b) for b in test_iter):
         out = greedy_decode(model, TEXT.vocab, batch.src, batch.src_mask)
         # print("SRC OUT: ", src.shape, out.shape)
         probs = model.generator(out)
         _, pred = torch.max(probs, dim = -1)
 
-        trans = [[TEXT.vocab.itos[word] for word in words] for words in pred]
-        source = [[TEXT.vocab.itos[word] for word in words] for words in batch.src]
-        target = [[TEXT.vocab.itos[word] for word in words] for words in batch.trg]
-        print("Source:", ' '.join(source[0]).split('</s>')[0])
-        print("Target:", ' '.join(target[0]).split('</s>')[0])
-        print("Translation:", ' '.join(trans[0]).split('</s>')[0])
-        print()
+        source = [[TEXT.vocab.itos[word] for word in words[1:]] for words in batch.src]
+        target = [[TEXT.vocab.itos[word] for word in words[1:]] for words in batch.trg]
+        translation = [[TEXT.vocab.itos[word] for word in words] for words in pred]
 
-    sys.exit()
+        for i in range(len(translation)):
+            src = ' '.join(source[i]).split('</s>')[0]
+            trg = ' '.join(target[i]).split('</s>')[0]
+            pred = ' '.join(translation[i]).split('</s>')[0]
 
-    # f_src = open(os.path.join(eval_dir, 'lang8.eval.src'), 'w+')
-    # f_trg = open(os.path.join(eval_dir, 'lang8.eval.trg'), 'w+')
-    # f_pred = open(os.path.join(eval_dir, 'lang8.eval.pred'), 'w+')
+            if '<unk>' in src or '<unk>' in trg:
+                continue
 
-    # for i, batch in enumerate(test_iter):
-        # # source
-        # source = ""
-        # for i in range(1, batch.src.size(0)):
-            # sym = TEXT.vocab.itos[batch.src.data[i, 0]]
-            # print("Batch.src.data ", batch.src.data, batch.src.data[i, 0])
-            # if sym == "</s>": break
-            # source += sym + " "
-        # source += '\n'
-        # if '<unk>' in source: continue
+            print("Source:", src)
+            print("Target:", trg)
+            print("Translation:", pred)
+            print()
 
-        # # target 
-        # target = ""
-        # for i in range(1, batch.trg.size(0)):
-            # sym = TEXT.vocab.itos[batch.trg.data[i, 0]]
-            # if sym == "</s>": break
-            # target += sym + " "
-        # target += '\n'
-        # if '<unk>' in target: continue
+            f_src.write(src + '\n')
+            f_trg.write(trg + '\n')
+            f_pred.write(pred + '\n')
 
-        # # translation 
-        # src_mask = (src != TEXT.vocab.stoi["<blank>"]).unsqueeze(-2)
-
-        # if 'elmo' in EMB:
-            # sentences = []
-            # for i in range(len(src)):
-                # sentences.append([TEXT.vocab.itos[id.item()] for id in src[i]])
-            # src = batch_to_ids(sentences).to(device)
-            # print(sentences)
-        # print(src.shape, src_mask.shape)
-
-        # out = greedy_decode(model, src, src_mask, 
-                            # max_len=60, start_symbol=TEXT.vocab.stoi["<s>"])
-        # pred = ""
-        # for i in range(1, out.size(1)):
-            # sym = TEXT.vocab.itos[out[0, i]]
-            # if sym == "</s>": break
-            # pred += sym + " "
-        # pred += '\n'
-
-        # print("Source:", source, end='')
-        # print("Target:", target, end='')
-        # print("Translation:", pred)
-        # f_src.write(source)
-        # f_trg.write(target)
-        # f_pred.write(pred)
-
-    # f_src.close()
-    # f_trg.close()
-    # f_pred.close()
+    f_src.close()
+    f_trg.close()
+    f_pred.close()
 
 if __name__ == "__main__":
     main()
